21ba804e95968eed3247d036d076354db6db989d
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-07 20:42:20 -0400

    Instalar adminlte
    Se realizó la instalación de adminlte via composer: https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Installation

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,19 @@
-Prueba
+@extends('adminlte::page')
+
+@section('title', 'MHenriquez')
+
+@section('content_header')
+    <h1>MHenriquez</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
