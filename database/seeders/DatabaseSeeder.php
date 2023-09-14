7dd3a7a0c5ba7ae10ee8414685d3f8040325f897
Manuel Henriquez <manuelhm1993@gmail.com>
2023-09-14 12:06:08 -0400

    Hacer seeding de la BBDD con 101 usuarios de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        User::create([
+            'name'     => 'Manuel Henriquez',
+            'email'    => 'manuel@pruebas.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        User::factory(100)->create();
     }
 }
